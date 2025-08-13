654f8927b61904e173b1f4088a8e4f52990c0384
soda <114852162+s1223takuma@users.noreply.github.com>
2025-08-13 17:42:56 +0900

    狂人が人狼わかるかどうかを選択式に

--- discordbot.py
+++ discordbot.py
@@ -197,7 +197,7 @@ async def start_game(ctx):
     if lunatic:
         lunatic = lunatic[0]
         await gamestatus[guild_id]["player_channel"][lunatic].send("あなたは狂人です。人狼と協力して村人を排除してください。")
-        await gamestatus[guild_id]["player_channel"][lunatic].send(f"人狼は{', '.join([wolf.display_name for wolf in gamestatus[guild_id]['wolf_list']])} です。")
+        # await gamestatus[guild_id]["player_channel"][lunatic].send(f"人狼は{', '.join([wolf.display_name for wolf in gamestatus[guild_id]['wolf_list']])} です。")
     gamestatus[guild_id]["status"] = "夜ターン"
     await asyncio.sleep(2)
     await ctx.send("役職配布完了。夜ターン開始...")
@@ -214,16 +214,19 @@ async def night_phase(ctx):
     for user, role in gamestatus[guild_id]["roles"].items():
         if user in gamestatus[guild_id]["dead_players"]:
             continue
-        if role == "人狼":
-            tasks.append(send_target_selection(guild_id, user, gamestatus[guild_id]["players"], "襲撃"))
-        elif role == "占い師":
-            gamestatus[guild_id][f'占い{user}_target'] = []
-            tasks.append(send_target_selection(guild_id, user, gamestatus[guild_id]["players"], f"占い{user}"))
-        elif role == "騎士":
-            gamestatus[guild_id][f'騎士{user}_target'] = []
-            tasks.append(send_target_selection(guild_id, user, gamestatus[guild_id]["players"], "守護"))
+        if gamestatus[guild_id]['turn'] != 1:
+            if role == "人狼":
+                tasks.append(send_target_selection(guild_id, user, gamestatus[guild_id]["players"], "襲撃"))
+            elif role == "占い師":
+                gamestatus[guild_id][f'占い{user}_target'] = []
+                tasks.append(send_target_selection(guild_id, user, gamestatus[guild_id]["players"], f"占い{user}"))
+            elif role == "騎士":
+                gamestatus[guild_id][f'騎士{user}_target'] = []
+                tasks.append(send_target_selection(guild_id, user, gamestatus[guild_id]["players"], "守護"))
+            else:
+                tasks.append(gamestatus[guild_id]["player_channel"][user].send("夜ターンです。村人は何もできません。"))
         else:
-            tasks.append(gamestatus[guild_id]["player_channel"][user].send("夜ターンです。村人は何もできません。"))
+            await gamestatus[guild_id]["player_channel"][user].send("初日の夜は行動できません。")
     await asyncio.gather(*tasks)
     for user, role in gamestatus[guild_id]["roles"].items():
         if role == "占い師":
