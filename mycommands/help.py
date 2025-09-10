63cdbf8ef730601f5f74ed98ffa57d27675cfd09
soda <114852162+s1223takuma@users.noreply.github.com>
2025-09-10 11:13:27 +0900

    ヘルプコマンドに追加

--- mycommands/help.py
+++ mycommands/help.py
@@ -13,4 +13,6 @@ async def help(ctx):
     embed.add_field(name="!search [クエリ]", value="ウェブ検索を行います。", inline=False)
     embed.add_field(name="!searchnews [クエリ]", value="ニュース検索を行います。", inline=False)
     embed.add_field(name="!searchimage [クエリ]", value="画像検索を行います。", inline=False)
+    embed.add_field(name="!speaker [話者ID]", value="話者を指定します。0-88の範囲で指定してください。", inline=False)
+    embed.add_field(name="!speakers", value="利用可能な話者の一覧を表示します。", inline=False)
     await ctx.send(embed=embed)
