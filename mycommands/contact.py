b3e63d1cfcb99051a1fd01823619818e73bf8fa1
soda <114852162+s1223takuma@users.noreply.github.com>
2025-09-14 17:49:09 +0900

    お問い合わせのメッセージを少し変更

--- mycommands/contact.py
+++ mycommands/contact.py
@@ -11,4 +11,4 @@ async def contact(ctx,*,inquiry):
     channel = await ctx.guild.create_text_channel(f'お問い合わせ- {ctx.author.name}', overwrites=overwrites)
     embed = discord.Embed(title="新規お問い合わせ", description=inquiry)
     await channel.send(embed=embed)
-    await channel.send(f"こんにちは！\n{ctx.author.mention}さんのお問い合わせを受け付けました。{owner.mention}よりご連絡いたします。")
+    await channel.send(f"こんにちは！\n{ctx.author.mention}さんのお問い合わせを受け付けました。{owner.mention}よりご連絡いたします。\n{owner.mention}他に追加したい人がいれば追加お願いします。")
