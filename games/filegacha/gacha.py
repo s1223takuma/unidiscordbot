408fafe2eb269b0a48d12965f3bcb9bbbdd6da17
soda <114852162+s1223takuma@users.noreply.github.com>
2025-09-14 20:30:10 +0900

    ガチャのメッセージを少し変更

--- games/filegacha/gacha.py
+++ games/filegacha/gacha.py
@@ -5,4 +5,4 @@ path = "games/filegacha/files/"
 async def filegacha(ctx):
     files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     selected_file = random.choice(files)
-    await ctx.send(file=discord.File(path + selected_file))
+    await ctx.send(f"おめでとう{selected_file[0]}ランクが当たりました",file=discord.File(path + selected_file))
