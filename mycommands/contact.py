10db680848ac60876d44eeccecaa7f91303d6e70
soda <114852162+s1223takuma@users.noreply.github.com>
2025-09-14 17:46:41 +0900

    お問い合わせのバグを修正

--- mycommands/contact.py
+++ mycommands/contact.py
@@ -2,14 +2,13 @@ import discord
 
 async def contact(ctx,*,inquiry):
     print(inquiry)
-    managementrole = discord.utils.get(ctx.guild.roles, name='bots')
+    owner = ctx.guild.owner
     overwrites = {
         ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
         ctx.author: discord.PermissionOverwrite(read_messages=True),
-        managementrole: discord.PermissionOverwrite(read_messages=True),
+        owner: discord.PermissionOverwrite(read_messages=True)
     }
     channel = await ctx.guild.create_text_channel(f'お問い合わせ- {ctx.author.name}', overwrites=overwrites)
-
     embed = discord.Embed(title="新規お問い合わせ", description=inquiry)
     await channel.send(embed=embed)
-    await channel.send(f"こんにちは！\n{ctx.author.mention}さんのお問い合わせを受け付けました。{managementrole.mention}よりご連絡いたします。")
+    await channel.send(f"こんにちは！\n{ctx.author.mention}さんのお問い合わせを受け付けました。{owner.mention}よりご連絡いたします。")
