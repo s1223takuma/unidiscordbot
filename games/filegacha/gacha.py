81feae51b2cd4b500ae118b64438b63bd2a611f2
soda <114852162+s1223takuma@users.noreply.github.com>
2025-09-14 19:58:00 +0900

    ちょっと変更

--- games/filegacha/gacha.py
+++ games/filegacha/gacha.py
@@ -1,7 +1,8 @@
 import random
 import discord
+import os
 path = "games/filegacha/files/"
 async def filegacha(ctx):
-    files = ["a.py","b.php","c.swift"]
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     selected_file = random.choice(files)
     await ctx.send(file=discord.File(path + selected_file))
